50c5e5b03a337c1d057cb8297309dfeef8beebfd
mohammed ahmed <mohammed@kali.mohammedahmed.com>
2024-08-03 21:03:57 +0300

    fix nvim link

---
+++ .devcontainer/nvim-config/lua/plugins/init.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+   {
+     "neovim/nvim-lspconfig",
+     config = function()
+       require("nvchad.configs.lspconfig").defaults()
+       require "configs.lspconfig"
+     end,
+   },
+   {
+   	"williamboman/mason.nvim",
+   	opts = {
+   		ensure_installed = {
+   			"lua-language-server", "stylua",
+   			"html-lsp", "css-lsp" , "prettier",
+        "typescript-language-server", "goimports", "gopls"
+   		},
+   	},
+   },
+   {
+   	"nvim-treesitter/nvim-treesitter",
+   	opts = {
+   		ensure_installed = {
+   			"vim", "lua", "vimdoc",
+        "html", "css", 'go'
+   		},
+    },
+   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+}
